280bdca7555bc03cc7f6534e2f3656aab8debf70
Rafael Bomfim <rafinhapbomfim@gmail.com>
2021-08-17 10:57:09 -0300

    17/08/2021

---
+++ estrutura_selecao/funcoes.js
@@ -0,0 +1,40 @@
+function exe1() {
+    //recuperar entrada de dados
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    let nota3 = Number(document.getElementById("nota3").value)
+    let nota4 = Number(document.getElementById("nota4").value)
+
+    //calcular média
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+    if (media >= 7){
+        document.getElementById("resultado").innerHTML = `A média é ${media} está aprovado`
+    }
+    else {
+        document.getElementById("resultado").innerHTML = `A média é ${media} está reprovado`
+    }
+}
+
+function exe2() {
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+
+    let media = (nota1 + nota2) / 2
+    let resultado
+
+    if ((media >=0) && (media < 3)){
+        resultado = `Aluno reprovado com média ${media}`
+    }
+    else if ((media >= 3) && (medai < 7)) {
+        resultado = `Aluno em exame com média ${media}`
+    }
+    else if ((media >= 7) && (medai <= 10)) {
+        resultado = `Aluno aprovado com média ${media}`
+    }
+    else {
+        resultado = `Notas informadas são inválidas`
+    }
+    //mostra reultado
+    document.getElementById("resultado").innerHTML = resultado
+}
